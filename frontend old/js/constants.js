476f6a5ebf0e91f50b3c0903f2e8df10e139b009
BigAshir <mrsofteeeaa@outlook.com>
2022-04-20 03:38:54 +0100

    test

--- frontend old/js/constants.js
+++ frontend old/js/constants.js
@@ -1,9 +1,8 @@
 const contractAddress = "0x53B36fB63C8f74993504c353777ccb9209407bB5"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
-
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+n
+const welcome_h1 = "Welcome to the Mashi NFT Project!";
+const welcome_p = 'Made with love!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
